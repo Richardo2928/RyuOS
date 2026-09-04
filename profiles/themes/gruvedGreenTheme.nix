c77ef8b1a885618f7d92dd2c3c962c668c9292b1
Ricardo Moreno <ricardo.2928rl@gmail.com>
2026-09-03 21:27:51 -0600

    move the theme to the profiles directory

---
+++ profiles/themes/gruvedGreenTheme.nix
@@ -0,0 +1,70 @@
+{
+  # Colorscheme: Guved Green
+  bg_hard = "1d2021";       # Blacker background; codeboxes
+  bg_primary = "282828";    # Main background
+  bg_soft = "32302f";       # Lateral panels; header
+  bg_elevated = "3c3836";   # Butons; elevated elements
+  bg_high = "504945";       # Borders; selections; inactive
+  bg_subtle = "665c54";     # Inactive lines
+
+  # Foregrounds
+  fg_muted = "7c6f64";      # Comments; deactivated text
+  fg_primary = "ebdbb2";    # Main text
+  fg_subtle = "928374";     # Punctuation; secundary text
+  fg_secundary = "a89984";  # Tertiary text
+  fg_bright = "fbf1c7";     # Bright text; foreground
+  fg_lightest = "f0f5d7";   # Lighter text
+
+  # Main Accents
+  green_deep = "0fa17a";    # Main dark accent; focused border
+  green_forest = "427b58";  # For some light mode
+  green_medium = "49ca94";  # Main Accent; active workspace
+  green_aqua = "71dda0";    # "Variable 3"; "graph arrows"...some shi like that
+  green_mint = "a4eca8";    # Soft accent; header text; properties
+  green_light = "d8f8b0";   # Highlights; "built-ins"
+  green_pale = "d8f8a8";    # "Extreme" light green
+
+  # Reds (Errors & alerts)
+  red_dark = "9d0006";      # Keywords, "errors" (some light mode I guess)
+  red_primary = "cc241d";   # Errors; "unresolved"
+  red_bright = "fb4934";     # Urgent alerts; keywords; operators
+
+  # Yellows (warnings & "types")
+  yellow_dark = "79740r";   # Types; strings (light mode I guess)
+  yellow_medium = "b57614"; # Functions; numbers (ligh mode again)
+  yellow_warm = "d79921";   # Intermediate warnings
+  yellow_primary = "fabd2f";# Types; functions; toolbar
+  yellow_pale = "d8f8b0";    # "Extreme" light yellow
+
+  # Oranges
+  orange_dark = "af3a03";   # Important; qualifiers (light mode)
+  orange_medium = "d65d0e"; # Secundary alerts
+  orange_primary = "fe8019";# Important; cursor; qualifiers
+  orange_soft = "d58c5d";   # Soft orange; neutral alt
+
+  # Blues & Cyans
+  blue_dark = "076678";     # Variables; meta (light mode)
+  blue_medium = "458588";   # Base blue Gruvbox
+  blue_primary = "83a598";  # Variables; neutral; meta
+  cyan_dark = "0fa17a";     # Dark cyan (light mode)
+  cyan_medium = "71dda0";   # Base cyan Gruvbox
+  cyan_primary = "8ec07c";  # "Variable 2"
+
+  # Megentas & Purples
+  purple_dark = "8f3f71";   # Numbers; "atoms" (light mode)
+  purple_medium = "b16286"; # Base purple Gruvbox
+  purple_primary = "d3869b";# Numbers; "atoms"; magenta terminal
+
+  # Blacks & Grays
+  black_hard = "141414";    # Pure black (avoid with text)
+  black_primary = "282828"; # Shared with bg_primary
+
+  # Light Backgrounds (Light Mode)
+  light_hard = "f9f5d7";    # Lighter background; modals
+  light_primary = "fbf1c7"; # Main background light mode
+  light_soft = "f2e6bc";    # Panels; codeboxes
+  light_medium = "ebdbb2";  # Cards; secundary alt
+  light_border = "d5c4a1";  # Borders; dividers
+  light_muted = "bdae93";   # Sutil borders; "graph lines"
+
+}
