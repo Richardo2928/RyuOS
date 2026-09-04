e2b822b736c8c9cacb9ab1c9385a2fd3e9958575
Ricardo Moreno <ricardo.2928rl@gmail.com>
2026-09-03 21:35:42 -0600

    refactor: clean up flake.nix structure and add the new outputs, 'laptop' & 'rdebian'

--- flake.nix
+++ flake.nix
@@ -1,48 +1,58 @@
 {
-	description = "Reproducible NixOS and Home Manager configuration";
+  description = "Reproducible NixOS and Home Manager configuration";
 
-	inputs = {
-		# NixOS packages channel
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    # NixOS packages channel
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-		# Home Manager Repository
-		home-manager = {
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";
-    	};
+    # Home Manager Repository
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-		# Opera flake by YisuiDenghua
-		opera-flake = {
-		#url = "github:yisuidenghua/opera-flake";
-		url = "path:/home/ricardo/opera-flake";
-		inputs.nixpkgs.follows = "nixpkgs";
-		};
-  	};
+    # Opera flake by YisuiDenghua
+    opera-flake = {
+      #url = "github:yisuidenghua/opera-flake";
+      url = "path:/home/ricardo/opera-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-	outputs = { self, nixpkgs, home-manager, opera-flake,...}@inputs: {
-		nixosConfigurations = {
-			# da name
-			daLaptop = nixpkgs.lib.nixosSystem {
-				system = "x86_64-linux";
-				modules = [
-					# System base config
-					./hosts/laptop/configuration.nix
+  outputs = { self, nixpkgs, home-manager, opera-flake, ... }@inputs:
+  let
+    system = "x86_64-linux";
 
-					# Home Manager integration
-					home-manager.nixosModules.home-manager
-					{
-						home-manager.useGlobalPkgs = true;
-						home-manager.useUserPackages = true;
-						home-manager.users.ricardo = import ./home/home.nix;
-					}
+    # pkgs con el overlay de Opera ya aplicado, para usar en homeConfigurations standalone
+    pkgsWithOverlays = import nixpkgs {
+      inherit system;
+      overlays = [ opera-flake.overlays.default ];
+    };
+  in {
+    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        # System base config
+        ./hosts/laptop/configuration.nix
 
-					# Opera overlay and enable propietary software
-					{
-						nixpkgs.overlays = [ opera-flake.overlays.default ];
-						nixpkgs.config.allowUnfree = true;
-					}
-				];
-			};
-		};
-	};
+        # Home Manager integration
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.ricardo = import ./homes/laptop.nix;
+        }
+
+        # Opera overlay
+        {
+          nixpkgs.overlays = [ opera-flake.overlays.default ];
+        }
+      ];
+    };
+
+    homeConfigurations.rDebian = home-manager.lib.homeManagerConfiguration {
+      pkgs = pkgsWithOverlays;
+      modules = [ ./homes/rdebian.nix ];
+    };
+  };
 }
